517bfd5efddc8b389b6da1c2cca6488eb39a42b7
bf_tsai <tsai19911026@gmail.com>
2023-07-21 14:22:02 +0800

    second page

---
+++ assets/js/all.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    // $('.header .test').click(function (e) { 
+    //     e.preventDefault();
+    //     $('.index-content').fadeToggle(1000);
+    // });
+});
--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 import './assets/scss/all.scss';
 
 console.log("Hello world!");
+
+$(document).ready(function () {
+    $('.allProduct').click(function (e) { 
+        e.preventDefault();
+        $('.index-content').fadeToggle(500);
+        $('.allProduct-content').delay(300).fadeToggle(500);
+    });
+});

c0b29c7fd081f3565dc7ef4fbbf9461feacf1519
bf_tsai <tsai19911026@gmail.com>
2023-07-27 22:28:07 +0800

    adjust jQuery

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,12 +1,3 @@
 
 
 
-$(document).ready(function () {
-    $(document).ready(function () {
-        // $('.allProduct').click(function (e) { 
-        //     e.preventDefault();
-        //     $('.index-content').fadeToggle(500);
-        //     $('.allProduct-content').delay(300).fadeToggle(500);
-        // });
-    });
-});
--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 import './assets/scss/all.scss';
 
+import './assets/js/index';
+import './assets/js/product';
+import './assets/js/shop';
+import './assets/js/faq';
+import './assets/js/blog';
+
 console.log("Hello world!");
 

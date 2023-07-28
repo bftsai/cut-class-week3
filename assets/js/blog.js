85603e1d7982b497e2eb69a305531edee9bdc017
bf_tsai <tsai19911026@gmail.com>
2023-07-28 22:14:58 +0800

    finish

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,7 +1,17 @@
 $(document).ready(function () {
     $('.more-1').click(function (e) { 
         e.preventDefault();
-        $('.blogContent').fadeOut(500);
+        $('.blog').fadeOut(500);
+        $('.more1Content').fadeIn(1000);
+    });
+    $('.ellipsisAfter-more.item1').click(function (e) { 
+        e.preventDefault();
+        $('.blog').fadeOut(500);
+        $('.more1Content').fadeIn(1000);
+    });
+    $('.blogContent .item1 h2,img').click(function (e) { 
+        e.preventDefault();
+        $('.blog').fadeOut(500);
         $('.more1Content').fadeIn(1000);
     });
 });
